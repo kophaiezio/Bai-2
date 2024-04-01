a9679b4ead7809283a787bd1b2cd96f3dec4084e
kophaiezio <ngocthang2332005@gmail.com>
2024-04-01 16:18:05 +0700

    First commit

---
+++ main.js
@@ -0,0 +1,28 @@
+
+class sv {
+    constructor(fullname, id, age, born, lopid, diem) {
+        this.fullname = fullname;
+        this.id = id;
+        this.age = age;
+        this.born = born;
+        this.lopid = lopid;
+        this.diem = diem;
+    }
+}
+
+
+const thongtinsinhvien1 = new sv("nguyen van a", "B10001", "18", "ha tinh", "D23CC03", "3")
+const thongtinsinhvien2 = new sv("nguyen van b", "B10002", "18", "ha noi", "D23CC03", "3")
+const thongtinsinhvien3 = new sv("nguyen van c", "B10003", "18", "hai phong", "D23CC03", "3")
+const thongtinsinhvien4 = new sv("nguyen van d", "B10004", "18", "ha noi", "D23CC03", "2.5")
+const thongtinsinhvien5 = new sv("nguyen van e", "B10005", "18", "nghe an", "D23CC03", "3.5")
+
+
+password= id;
+
+var admin = [{name: 'le van luyen', password: '1234567'}];
+
+
+function login(name, password) {
+    for (var i = o; i < student)
+}
